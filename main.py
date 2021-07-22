21d26c0f52b56b36519cf70d75da612a721b8882
Ivan Goister <gesto@Ivans-MacBook-Air.local>
2021-07-22 19:24:43 +0300

    Creating directory

---
+++ main.py
@@ -0,0 +1,31 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from configuration import API_TOKEN
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+
+    await message.reply("Цей бот ще маленький, і вміє лише повторювати написаний текст")
+
+@dp.message_handler(commands=['stop'])
+async def send_welcome(message: types.Message):
+
+    await message.reply("Цього бота просто так не зупинити")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+
+    await message.answer(message.text)
+    print(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
+
