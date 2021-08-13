1e758cdc5a9e5a5c2d17cfaf06bd1d9031eb08cc
IvanGoister <ivan.goister@gmail.com>
2021-08-13 22:55:53 +0300

    Bot can get user's information from two questions and return it for check. Code have a lot of useless comments and text.

--- Customer_Information.py
+++ Customer_Information.py
@@ -1,3 +1,60 @@
-name = ''
-height = 0
+from main import *
+import main
+#from aiogram import types
+from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
 
+class CustomerInfo(StatesGroup):
+    Name = State()
+    Phone_number = State()
+    Weight = State()
+    BraSize = State()
+    AssSize = State()
+    EyeColor = State()
+    Nationality = State()
+    Religion = State()
+
+@dp.message_handler(commands=['info'], state=None)
+async def enter_test(message: types.Message):
+    await bot.send_message(message.from_user.id, "Заповніть дані про себе\n Ваше ім'я")
+    await CustomerInfo.Name.set()
+@dp.message_handler(state=CustomerInfo.Name)
+async def answer_Name(message: types.Message, state: FSMContext):
+    answer=message.text
+    await state.update_data(answer_Name=answer)
+
+    await bot.send_message(message.from_user.id, "Заповніть дані про себе\n Номер телефону")
+    await CustomerInfo.Phone_number.set()
+
+@dp.message_handler(state=CustomerInfo.Phone_number)
+async def answer_Phone_number(message: types.Message, state: FSMContext):
+    answer = message.text
+    await state.update_data(answer_Phone_number=answer)
+
+    await bot.send_message(message.from_user.id, "Ваші дані прийняті 👌")
+    await state.finish()
+
+
+def oprosnik():
+    @dp.message_handler(commands=['info'], state=None)
+    async def enter_test(message: types.Message):
+        await bot.send_message(message.from_user.id, "Заповніть дані про себе\n Ваше ім'я")
+        await CustomerInfo.Name.set()
+
+    @dp.message_handler(state=CustomerInfo.Name)
+    async def answer_Name(message: types.Message, state: FSMContext):
+        answer = message.text
+        await state.update_data(answer_Name=answer)
+
+        await bot.send_message(message.from_user.id, "Заповніть дані про себе\n Номер телефону")
+        await CustomerInfo.Phone_number.set()
+
+    @dp.message_handler(state=CustomerInfo.Phone_number)
+    async def answer_Phone_number(message: types.Message, state: FSMContext):
+        answer = message.text
+        await state.update_data(answer_Phone_number=answer)
+
+        await bot.send_message(message.from_user.id, "Ваші дані прийняті 👌")
+        await state.finish()
+    return CustomerInfo
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import logging
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+import Customer_Information
 from configuration import API_TOKEN, TikTokLink, InstaLink
 import markups as NavButt
+from Customer_Information import *
+
+
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+
+
+
+class CastomerInfo(StatesGroup):
+    Name = State()
+    Phone_number = State()
+    Weight = State()
+    BraSize = State()
+    AssSize = State()
+    EyeColor = State()
+    Nationality = State()
+    Religion = State()
 
 
 
@@ -10,7 +30,8 @@ logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -18,9 +39,9 @@ async def send_welcome(message: types.Message):
     await bot.send_message(message.from_user.id, 'Привіт {0.first_name}, цей бот ще маленький, і поки що нічого не вміє'.format(message.from_user), reply_markup=NavButt.MainMenu)
 
 @dp.message_handler(commands=['stop'])
-async def send_welcome(message: types.Message):
+async def send_welcom(message: types.Message):
 
-    await bot.send_message("Цього бота просто так не зупинити")
+    await bot.send_message(message.from_user.id, "Цього бота просто так не зупинити")
 
 
 @dp.message_handler()
@@ -29,7 +50,36 @@ async def bot_message(message: types.Message):
     #await message.answer("message.text")
     #print(message.text)
     if message.text == '🪡 Ввести параметри тіла':
-        await bot.send_message(message.from_user.id, 'Можете ввести параметри тіла, але поки що я з ними нічого зробити не можу 🤷‍♀️')
+
+        #await bot.send_message(message.from_user.id, 'Можете ввести параметри тіла, але поки що я з ними нічого зробити не можу 🤷‍♀️')
+
+        await bot.send_message(message.from_user.id, "Заповніть дані про себе\n Ваше ім'я")
+        await CastomerInfo.Name.set()
+
+        @dp.message_handler(state=CastomerInfo.Name)
+        async def answer_Name(message: types.Message, state: FSMContext):
+            answer = message.text
+            await state.update_data(answer_Name=answer)
+
+            await bot.send_message(message.from_user.id, "Номер телефону")
+            await CastomerInfo.Phone_number.set()
+
+        @dp.message_handler(state=CastomerInfo.Phone_number)
+        async def answer_Phone_number(message: types.Message, state: FSMContext):
+            answer = message.text
+            await state.update_data(answer_Phone_number=answer)
+
+            await bot.send_message(message.from_user.id, "Ваші дані прийняті 👌")
+            data=await state.get_data()
+            name=data.get('answer_Name')
+            number=data.get('answer_Phone_number')
+            await bot.send_message(message.from_user.id, "{Name}, ваш номер {Number}".format(Name=name, Number=number))
+            print(message.from_user.id, message.from_user.first_name, " --write-- "+name+' '+number)
+            await state.finish()
+
+
+
+
 
     elif message.text == '🧷 Посилання':
         await bot.send_message(message.from_user.id, 'Посилання \n ⬇️⬇️⬇️️️', reply_markup=NavButt.LinksMenu)
@@ -41,6 +91,41 @@ async def bot_message(message: types.Message):
         await bot.send_message(message.from_user.id, TikTokLink)
 
     print(message.from_user.id, message.from_user.first_name, " --write-- ", message.text)
+
+@dp.message_handler()
+async def bot_message(message: types.Message):
+    if message.text == '🪡 Ввести параметри тіла':
+        await bot.send_message(message.from_user.id, 'Можете !!!!ввести параметри тіла, але поки що я з ними нічого зробити не можу 🤷‍♀️')
+
+"""
+@dp.message_handler(commands=['info'])
+async def enter_test(message: types.Message):
+    await bot.send_message(message.from_user.id, "Заповніть дані про себе\n Ваше ім'я")
+    await CastomerInfo.Name.set()
+@dp.message_handler(state=CastomerInfo.Name)
+async def answer_Name(message: types.Message, state: FSMContext):
+    answer=message.text
+    await state.update_data(answer_Name=answer)
+
+    await bot.send_message(message.from_user.id, "Заповніть дані про себе\n Номер телефону")
+    await CastomerInfo.Phone_number.set()
+
+@dp.message_handler(state=CastomerInfo.Phone_number)
+async def answer_Phone_number(message: types.Message, state: FSMContext):
+    answer = message.text
+    await state.update_data(answer_Phone_number=answer)
+
+    await bot.send_message(message.from_user.id, "Ваші дані прийняті 👌")
+    await state.finish()
+
+
+
+
+
+
+
+"""
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
